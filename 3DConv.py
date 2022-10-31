9edcadc0e3713aff09c6b67524fb24d4e6e7ee10
jakobfinnvold <jakobuf@uia.no>
2022-10-31 11:58:15 +0100

    New changes

--- 3DConv.py
+++ 3DConv.py
@@ -1,9 +1,39 @@
+import numpy as np
+
 from getLocationandWeather import WeatherSymbol
 import requests
 import numpy
+import matplotlib as mpl
+from PIL import Image
 
 with open("WeatherSymbol.txt") as f:
-    symbol = f.readlines()
+    symbol = f.readline()
+    f.close()
+
+weather_symbol = f'./png/{symbol}.png'
+
+# Open image and convert to grey scale
+img = Image.open(weather_symbol).convert('LA')
+
+if IOError == 0:
+    print("Error Importing Image")
+
+# Defining verticies
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
--- master.py
+++ master.py
@@ -1,5 +1,6 @@
 import requests as rq
 import time
+from PIL import Image
 from getLocationandWeather import WeatherSymbol
 
 location = input("Enter your desired location!: ")
@@ -8,8 +9,24 @@ my_location = WeatherSymbol(location)
 print(my_location.get_weather_symbol())
 
 with open("WeatherSymbol.txt") as f:
-    symbol = f.readlines()
-    print(symbol)
+    symbol = f.readline()
+
+    f.close()
+
+
+weather_symbol = f'./png/{symbol}.png'
+
+print(weather_symbol)
+
+try:
+    img = Image.open(weather_symbol).convert('LA')
+except IOError:
+        print("Error Importing Image")
+
+r = img.show(img)
+
+
+
 
 
 
